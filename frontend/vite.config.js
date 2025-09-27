2fc869b31fd8e6523e91149de7ac0c7782454953
Fardin A. <fardinarafat30@gmail.com>
2025-09-27 02:26:00 +0200

    Configure allowed hosts in Vite server settings
    Added allowed hosts for the development server configuration.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/',
+  server: {
+    allowedHosts: ['ok2buy-1.onrender.com', 'localhost'], // Add your allowed hosts here
+  },
   plugins: [react()],
   host: true,
   resolve: {
